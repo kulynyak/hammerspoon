2b856dd2381e3f3843c2f3400fd681d1be0cea84
Andriy Kulynyak <kulynyak@gmail.com>
2020-03-28 11:48:28 +0200

    modify launch-apps implementation

---
+++ launch-apps.lua
@@ -0,0 +1,14 @@
+return {
+  -- { app-path, bundleID, hide}, -- comment
+  { 'Notes', 'com.apple.Notes', true },
+  { 'MacPass', 'com.hicknhacksoftware.MacPass', true },
+  { 'Slack', 'com.tinyspeck.slackmacgap', true },
+  { 'Viber', 'com.viber.osx', true },
+  { 'WhatsApp', 'WhatsApp', true },
+  { 'Telegram', 'ru.keepcoder.Telegram', true },
+  { 'com.flexibits.fantastical2.mac', 'com.flexibits.fantastical2.mac', true },
+  -- { 'Mail', 'com.apple.mail', true },
+  { 'Unibox', 'com.eightloops.Unibox', true },
+  { 'Skype', 'com.skype.skype', true},
+  { 'Microsoft Teams', 'com.microsoft.teams', true },
+}
