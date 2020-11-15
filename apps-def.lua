cd12c506e7fbf28f10398bc07c86a51c94741b4d
Andriy Kulynyak <kulynyak@gmail.com>
2020-11-15 18:40:34 +0200

    Misc changes

---
+++ apps-def.lua
@@ -0,0 +1,36 @@
+-- Keybindings for launching apps in Hyper Mode
+
+return {
+  -- { hotkey, app-path, kbd-layout },                                              -- comment
+  { '2', '/Applications/Postman.app', 'en' },                                       -- "4" for "Postman"
+  { '3', '/Applications/Robo 3T.app', 'en' },                                       -- "3" for "Robo 3T"
+  -- { '4', '/Applications/ShowyEdge.app', 'en' },                                    -- "4" for "ShowyEdge"
+  { 'a', '/System/Applications/Utilities/Activity Monitor.app', 'en' },             -- "A" for "Activity Monitor"
+  { 'b', '/Applications/Brave Browser.app', nil},                                   -- "B" for "Browser"
+  { 'c', '/Applications/VSCodium.app', 'en' },                                      -- "C for "VSCodium"
+  { 'd', '/Applications/Firefox.app', nil },                                        -- "D" for "Development"
+  { 'e', '/Applications/Firefox Developer Edition.app', nil },                      -- "E" for "Editionh"
+  { 'f', '/System/Library/CoreServices/Finder.app', 'en' },                         -- "F" for "Finder"
+  { 'g', '/Applications/Google Chrome.app', nil },                                  -- "G" for "Google"
+  { nil, '/Applications/Safari.app', nil },                                         -- "H" for "Safari"
+  { 'i', '/Applications/IntelliJ IDEA.app', 'en' },                                 -- "I" for "InwtelliJ IDEA"
+  { 'j', '/Applications/Cisco/Cisco AnyConnect Secure Mobility Client.app', 'en' }, -- "J" for "Join VPN"
+  { nil, '/Applications/MongoDB Compass.app', 'en' },                               -- "K" for "Compass"
+  { 'l', '/Applications/Slack.app', nil },                                          -- "L" for "Slack"
+  { 'm', '/System/Applications/Mail.app', 'en' },                                   -- "M" for "Mail"
+  { nil, '/Applications/Android Studio.app', 'en' },                                -- "N" for "Android"
+  { 'o', '/Applications/Opera.app', nil },                                          -- "O" for "Opera"
+  { 'p', '/Applications/MacPass.app', 'en' },                                       -- "P" for "MacPass"
+  { nil, '/System/Applications/QuickTime Player.app', nil},                         -- "q" for Quick Time
+  { 'r', '/Applications/Viber.app', 'uk' },                                         -- "V" for "Viber"
+  { 's', '/Applications/Sourcetree.app', 'en'},                                     -- "S" for "Sourcetree"
+  { nil, '/Applications/Skype.app', 'uk' },                                         -- " " for "Skype"
+  { nil, '/System/Applications/Utilities/Terminal.app', 'en' },                     -- " " for "Terminal"
+  { 'return', '/Applications/Alacritty.app', 'en' },                                -- "↲" for "Alacritty"
+  { nil, '/Applications/kitty.app', 'en' },                                       -- "↲" for "Kitty"
+  { 't', '/Applications/Telegram.app', nil, },                                      -- "T" for Telegram
+  { 'w', '/Applications/WhatsApp.app', 'uk' },                                      -- "W" for "WhatsApp"
+  { 'v', '/Applications/Vivaldi.app', nil },                                        -- "X" for "Another Browser"
+  { 'z', '/Applications/zoom.us.app', 'en' },                                       -- "Z" for "Zoom US"
+  { '4', '/Applications/Microsoft Teams.app', 'en' }                               
+}
